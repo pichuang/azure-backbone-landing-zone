6fcf723123e3262aa122863ceb14cba247184a74
Phil Huang <phil.huang@microsoft.com>
2025-12-06 22:21:14 +0800

    🔧 Improve error handling in Terragrunt script by refining input validation for target directory.
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- 4-terragrunt-local-dev-apply-specific.sh
+++ 4-terragrunt-local-dev-apply-specific.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # 使用 Local State
+set -euo pipefail
+
 export TG_LOCAL_STATE=true
 export TF_VAR_use_msi=false
 export TF_VAR_use_cli=true
 
 TARGET_DIR=""
 
-if [ -z "$1" && -z "$TARGET_DIR" ]; then
+if [[ -z "${1:-}" || "$1" == "0" ]]; then
   echo "請提供要執行的目錄路徑，範例：platform/connectivity/rg-bb-vwan-prd-global-01"
-else
-  TARGET_DIR="$1"
+  exit 1
 fi
+
+TARGET_DIR="$1"
 
 # 格式化 Terraform 配置文件
 terragrunt run -- fmt --recursive
