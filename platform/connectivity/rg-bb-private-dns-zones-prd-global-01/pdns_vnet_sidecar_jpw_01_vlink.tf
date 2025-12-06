ab02ebeb511c9d7ffa65a214e11aa4ae3faaeb45
Phil Huang <phil.huang@microsoft.com>
2025-12-06 23:24:38 +0800

    ✨ Add bastion private DNS zone configuration for improved network integration
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- platform/connectivity/rg-bb-private-dns-zones-prd-global-01/pdns_vnet_sidecar_jpw_01_vlink.tf
+++ platform/connectivity/rg-bb-private-dns-zones-prd-global-01/pdns_vnet_sidecar_jpw_01_vlink.tf
@@ -864,3 +864,12 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_service_si
   registration_enabled  = false
   resolution_policy     = local.pdns_vnet_sidecar_jpw_01_vlink.resolution_policy
 }
+
+resource "azurerm_private_dns_zone_virtual_network_link" "bastion_azure_com" {
+  name                  = "vlink-bastion-azure-com"
+  resource_group_name   = azurerm_resource_group.rg_private_dns_zones.name
+  private_dns_zone_name = azurerm_private_dns_zone.bastion_azure_com.name
+  virtual_network_id    = var.vnet_sidecar_jpw_01_id
+  registration_enabled  = false
+  resolution_policy     = local.pdns_vnet_sidecar_jpw_01_vlink.resolution_policy
+}
