a71074da8f10e88ec51a72a8c452182bdc7273b7
Phil Huang <phil.huang@microsoft.com>
2025-12-08 15:07:05 +0800

    🗑️ Remove deprecated APIM standard module and related configurations to streamline the deployment process and improve maintainability.
    Signed-off-by: Phil Huang <phil.huang@microsoft.com>

--- landingzones/apim-standard-v2/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  apim_standard_v2_jpw_dev = {
-    log_analytics_workspace_id = "/subscriptions/587f8045-973f-43b7-965c-d368921946a2/resourcegroups/rg-bb-log-soc-prd-jpw-01/providers/microsoft.operationalinsights/workspaces/log-soc-prd-jpw-01"
-    location                   = var.secondary_location
-  }
-}
--- landingzones/apim-standard-v2/rg-apim-standard-v2-jpw-dev.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg_apim_standard_v2_jpw_dev" {
-  location = var.secondary_location
-  name     = "rg-apim-standard-v2-jpw-dev"
-}
